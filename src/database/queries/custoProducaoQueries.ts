f6a876689217a96edac33644f5547b694cfc09f4
Iran Adryan <contatoiranadryan@gmail.com>
2023-02-15 16:16:12 -0300

    fix: change left join to inner join in custo producao queries to speed up

--- src/database/queries/custoProducaoQueries.ts
+++ src/database/queries/custoProducaoQueries.ts
@@ -10,8 +10,8 @@ select
     coalesce(cast(sum(abastecimento_ciclo_ts.valor) as numeric(15,2)), 0) as total,
     'Abastecimento' as categoria
 from abastecimento_ciclo_ts
-left join abastecimento_ciclo on abastecimento_ciclo.id = abastecimento_ciclo_ts.id_abastecimento_ciclo
-left join abastecimento on abastecimento.id = abastecimento_ciclo.id_abastecimento
+inner join abastecimento_ciclo on abastecimento_ciclo.id = abastecimento_ciclo_ts.id_abastecimento_ciclo
+inner join abastecimento on abastecimento.id = abastecimento_ciclo.id_abastecimento
 where abastecimento.status_processamento = 2
 and abastecimento_ciclo.id_ciclo_producao in (${idSafra})
 ${idTalhao ? `and abastecimento_ciclo_ts.id_talhao_safra = ${idTalhao}` : ''}
@@ -24,10 +24,10 @@ select
     coalesce(cast(sum(movimento_conta_ciclo_ts.valor) as numeric(15,2)), 0) as total,
     'Despesas Financeiras' as categoria
 from movimento_conta_ciclo_ts
-left join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
-left join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
-left join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
-left join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
+inner join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
+inner join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
+inner join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
+inner join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
 where movimento_conta_m.tipo_lancamento = 'D'
 and movimento_conta_apropriacao.apropriacao_custo = 1
 and movimento_conta_ciclo.id_ciclo_producao in (${idSafra})
@@ -46,7 +46,7 @@ from (
         sum(manutencao_m_ciclo_ts.valor) as total,
         'Despesas Patrimônio' as categoria
     from manutencao_m_ciclo_ts
-    left join manutencao_m_ciclo on manutencao_m_ciclo.id = manutencao_m_ciclo_ts.id_manutencao_m_ciclo
+    inner join manutencao_m_ciclo on manutencao_m_ciclo.id = manutencao_m_ciclo_ts.id_manutencao_m_ciclo
     where manutencao_m_ciclo.id_ciclo_producao in (${idSafra})
     ${idTalhao ? `and manutencao_m_ciclo_ts.id_talhao_safra = ${idTalhao}` : ''}
     ${startDate ? `and manutencao_m.data >= ${startDate}` : ''}
@@ -58,10 +58,10 @@ from (
         sum(movimento_conta_ciclo_ts.valor) as total,
         'Despesas Patrimônio' as categoria
     from movimento_conta_ciclo_ts
-    left join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
-    left join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
-    left join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
-    left join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
+    inner join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
+    inner join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
+    inner join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
+    inner join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
     where movimento_conta_m.tipo_lancamento = 'D'
     and movimento_conta_apropriacao.apropriacao_custo = 4
     and movimento_conta_ciclo.id_ciclo_producao in (${idSafra})
@@ -84,7 +84,7 @@ select
     ) as numeric(15,2)), 0),
     'Atividades Agrícolas' as categoria
 from agri_atv_talhao_safra
-left join agri_atv on agri_atv.id = agri_atv_talhao_safra.id_agri_atv
+inner join agri_atv on agri_atv.id = agri_atv_talhao_safra.id_agri_atv
 where agri_atv.id_ciclo_producao in (${idSafra})
 ${idTalhao ? `and agri_atv_talhao_safra.id_talhao_safra = ${idTalhao}` : ''}
 ${startDate ? `and agri_atv.data_inicio >= ${startDate}` : ''}
@@ -106,12 +106,12 @@ from(
     talhao_safra.hectares as area,
     ciclo_producao.nome as safra
   from abastecimento_ciclo_ts
-  left join abastecimento_ciclo on abastecimento_ciclo.id = abastecimento_ciclo_ts.id_abastecimento_ciclo
-  left join abastecimento on abastecimento.id = abastecimento_ciclo.id_abastecimento
-  left join talhao_safra on talhao_safra.id = abastecimento_ciclo_ts.id_talhao_safra
-  left join talhao on talhao.id = talhao_safra.id_talhao
-  left join variedade on variedade.id = talhao_safra.id_variedade
-  left join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
+  inner join abastecimento_ciclo on abastecimento_ciclo.id = abastecimento_ciclo_ts.id_abastecimento_ciclo
+  inner join abastecimento on abastecimento.id = abastecimento_ciclo.id_abastecimento
+  inner join talhao_safra on talhao_safra.id = abastecimento_ciclo_ts.id_talhao_safra
+  inner join talhao on talhao.id = talhao_safra.id_talhao
+  inner join variedade on variedade.id = talhao_safra.id_variedade
+  inner join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
   where abastecimento.status_processamento = 2
   and abastecimento_ciclo.id_ciclo_producao in (${idSafra})
   ${idTalhao ? `and abastecimento_ciclo_ts.id_talhao_safra = ${idTalhao}` : ''}
@@ -128,14 +128,14 @@ from(
     talhao_safra.hectares as area,
     ciclo_producao.nome as safra
   from movimento_conta_ciclo_ts
-  left join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
-  left join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
-  left join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
-  left join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
-  left join talhao_safra on talhao_safra.id = movimento_conta_ciclo_ts.id_talhao_safra
-  left join talhao on talhao.id = talhao_safra.id_talhao
-  left join variedade on variedade.id = talhao_safra.id_variedade
-  left join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
+  inner join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
+  inner join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
+  inner join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
+  inner join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
+  inner join talhao_safra on talhao_safra.id = movimento_conta_ciclo_ts.id_talhao_safra
+  inner join talhao on talhao.id = talhao_safra.id_talhao
+  inner join variedade on variedade.id = talhao_safra.id_variedade
+  inner join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
   where movimento_conta_m.tipo_lancamento = 'D'
   and movimento_conta_apropriacao.apropriacao_custo = 1
   and movimento_conta_ciclo.id_ciclo_producao in (${idSafra})
@@ -154,11 +154,11 @@ from(
     talhao_safra.hectares as area,
     ciclo_producao.nome as safra
   from manutencao_m_ciclo_ts
-  left join manutencao_m_ciclo on manutencao_m_ciclo.id = manutencao_m_ciclo_ts.id_manutencao_m_ciclo
-  left join talhao_safra on talhao_safra.id = manutencao_m_ciclo_ts.id_talhao_safra
-  left join talhao on talhao.id = talhao_safra.id_talhao
-  left join variedade on variedade.id = talhao_safra.id_variedade
-  left join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
+  inner join manutencao_m_ciclo on manutencao_m_ciclo.id = manutencao_m_ciclo_ts.id_manutencao_m_ciclo
+  inner join talhao_safra on talhao_safra.id = manutencao_m_ciclo_ts.id_talhao_safra
+  inner join talhao on talhao.id = talhao_safra.id_talhao
+  inner join variedade on variedade.id = talhao_safra.id_variedade
+  inner join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
   where manutencao_m_ciclo.id_ciclo_producao in (${idSafra})
   ${idTalhao ? `and manutencao_m_ciclo_ts.id_talhao_safra = ${idTalhao}` : ''}
   ${startDate ? `and manutencao_m.data >= ${startDate}` : ''}
@@ -174,14 +174,14 @@ from(
     talhao_safra.hectares as area,
     ciclo_producao.nome as safra
   from movimento_conta_ciclo_ts
-  left join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
-  left join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
-  left join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
-  left join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
-  left join talhao_safra on talhao_safra.id = movimento_conta_ciclo_ts.id_talhao_safra
-  left join talhao on talhao.id = talhao_safra.id_talhao
-  left join variedade on variedade.id = talhao_safra.id_variedade
-  left join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
+  inner join movimento_conta_ciclo on movimento_conta_ciclo.id = movimento_conta_ciclo_ts.id_movimento_conta_ciclo
+  inner join movimento_conta_apropriacao on movimento_conta_apropriacao.id = movimento_conta_ciclo.id_movimento_conta_apropriacao
+  inner join movimento_conta on movimento_conta.id = movimento_conta_apropriacao.id_movimento_conta
+  inner join movimento_conta_m on movimento_conta_m.id = movimento_conta.id_movimento_conta_m
+  inner join talhao_safra on talhao_safra.id = movimento_conta_ciclo_ts.id_talhao_safra
+  inner join talhao on talhao.id = talhao_safra.id_talhao
+  inner join variedade on variedade.id = talhao_safra.id_variedade
+  inner join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
   where movimento_conta_m.tipo_lancamento = 'D'
   and movimento_conta_apropriacao.apropriacao_custo = 4
   and movimento_conta_ciclo.id_ciclo_producao in (${idSafra})
@@ -207,11 +207,11 @@ from(
     talhao_safra.hectares as area,
     ciclo_producao.nome as safra
   from agri_atv_talhao_safra
-  left join agri_atv on agri_atv.id = agri_atv_talhao_safra.id_agri_atv
-  left join talhao_safra on talhao_safra.id = agri_atv_talhao_safra.id_talhao_safra
-  left join talhao on talhao.id = talhao_safra.id_talhao
-  left join variedade on variedade.id = talhao_safra.id_variedade
-  left join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
+  inner join agri_atv on agri_atv.id = agri_atv_talhao_safra.id_agri_atv
+  inner join talhao_safra on talhao_safra.id = agri_atv_talhao_safra.id_talhao_safra
+  inner join talhao on talhao.id = talhao_safra.id_talhao
+  inner join variedade on variedade.id = talhao_safra.id_variedade
+  inner join ciclo_producao on ciclo_producao.id = talhao_safra.id_ciclo_producao
   where agri_atv.id_ciclo_producao in (${idSafra})
   ${idTalhao ? `and agri_atv_talhao_safra.id_talhao_safra = ${idTalhao}` : ''}
   ${startDate ? `and agri_atv.data_inicio >= ${startDate}` : ''}
